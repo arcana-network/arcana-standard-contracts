b1a05adcb92718d1219d393ecbe0405f963902ac
Himank Jain <76067554+steel-feel@users.noreply.github.com>
2022-05-30 13:11:27 +0530

    added token URI

--- scripts/mint.js
+++ scripts/mint.js
@@ -3,7 +3,8 @@ const hre = require("hardhat");
 async function main() {
     
     const nftContractAddress = "0xcAf83A2c7755D79Ba5bC83346E4AE013d2D3254d",
-    tokenId = 2, to = "0xd018E133CeF28AE3F4F27b16F1AB43BBdd53BDcb"l
+    tokenId = 2, to = "0xd018E133CeF28AE3F4F27b16F1AB43BBdd53BDcb",
+    tokenUri= "sample.url/2";
     
     const ARC721_factory = await hre.ethers.getContractFactory("ARC721");
     const ARC721 = await ARC721_factory.attach(nftContractAddress)
